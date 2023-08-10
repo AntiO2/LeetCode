d47b20fd50fddcb69f25e41706c37c37abd4607b
AntiO2 <antio2@qq.com>
2023-08-11 00:25:43 +0800

    solution 283

---
+++ solution/0_sample.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Anti
+//
+#include "fmt/core.h"
+#include "gtest/gtest.h"
+#include "logger.h"
+
+
+TEST(SAMPLE,SAMPLE1) {
+    EXPECT_EQ(true,true);
+}
--- solution/top100/283_move-zeroes.cpp
+++ solution/top100/283_move-zeroes.cpp
@@ -1,3 +1,31 @@
 //
 // Created by Anti on 2023/8/11.
 //
+#include "fmt/core.h"
+#include "gtest/gtest.h"
+#include <vector>
+
+class Solution {
+public:
+    void moveZeroes(std::vector<int>& nums) {
+        auto maintainer = 0; // 从0-maintainer都是非0的数
+        auto searcher = 0; // searcher负责向右寻找
+        auto len = nums.size();
+        while (searcher < len) {
+            if(nums.at(searcher)!=0) {
+                std::swap(nums.at(maintainer),nums.at(searcher));
+                maintainer++;
+            }
+            searcher++;
+        }
+    }
+};
+
+TEST(MOVE_ZERO,SAMPLE1) {
+    std::vector<int> nums={0,1,0,3,12};
+    Solution s;
+    s.moveZeroes(nums);
+    std::for_each(nums.begin(),nums.end(),[](const int &num) {
+        fmt::print("{} ",num);
+    });
+}
