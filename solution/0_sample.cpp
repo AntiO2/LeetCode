27410bbe14b34d9517c67c9206b8c5c9046ecd81
AntiO2 <antio2@qq.com>
2023-09-26 22:36:56 +0800

    solution 2582

--- solution/0_sample.cpp
+++ solution/0_sample.cpp
@@ -16,8 +16,14 @@ TEST(test0,SAMPLE2) {
     Solution s;
 }
 
-TEST(test0,SAMPLE3) {
-    std::list<int > list = {1,3,5,7,9};
-    list.insert(list.end(),2);
-    EXPECT_EQ(list.size(),6);
+//TEST(test0,SAMPLE3) {
+//    std::list<int > list = {1,3,5,7,9};
+//    list.insert(list.end(),2);
+//    EXPECT_EQ(list.size(),6);
+//}
+
+
+TEST(test0,SAMPLE4) {
+    auto a = 9;
+    printf("%d %d\n", a = a+4,a*4);
 }
