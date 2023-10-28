56b214db975ca5ad64ec14218d577aecde67a70c
AntiO2 <antio2@qq.com>
2023-10-28 23:04:18 +0800

    feat(asan) add asan test sample

--- solution/0_sample.cpp
+++ solution/0_sample.cpp
@@ -8,7 +8,11 @@
 #include "gtest/gtest.h"
 #include "logger.h"
 
-class Solution {};  // fill in it
+class Solution {
+
+ public:
+  void test() { LOG_INFO("TEST SOL"); }
+};  // fill in it
 
 TEST(test0, SAMPLE1) {
   Solution sol;
@@ -16,4 +20,19 @@ TEST(test0, SAMPLE1) {
 
 TEST(test0, SAMPLE2) {
   Solution sol;
+}
+
+TEST(test0, DISABLED_ASAN_TEST) {
+  // 栈内存越界
+  int array[3]{0, 1, 2};
+  LOG_INFO("%d", array[3]);
+  // 堆内存越界
+  auto stack_array = new int[3];
+  LOG_INFO("%d", stack_array[3]);
+
+  auto memory_leak = new int[3];  // 内存泄露
+  // delete[] memory_leak;
+  auto s = new Solution();
+  delete s;
+  s->test();  // use after free;
 }
