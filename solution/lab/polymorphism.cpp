5345c202d8dd70baf3decd9b06fc8e2b33b674cb
AntiO2 <antio2@qq.com>
2023-11-07 00:18:28 +0800

    lab polymorphism

--- solution/lab/polymorphism.cpp
+++ solution/lab/polymorphism.cpp
@@ -34,3 +34,46 @@ TEST(TEST_POLY, DESTORY) {
   B* bp = new B();
   delete bp;
 }
+
+class C {
+ public:
+  template <typename T>
+  // virtual void func(){};
+  void func(){};
+};
+TEST(TEST_POLY, VIRTUAL) {
+  C c;
+  c.func<int>();
+  c.func<long>();
+}
+
+class Empty {};
+template <class E>
+class Empty2 {
+ public:
+  E e;
+};
+class Empty4 : Empty {
+  virtual void func(){};
+};
+class Empty5 : Empty {
+  void func(){};
+};
+TEST(TEST_EMPTY, SAMPLE) {
+  Empty e1;
+  LOG_INFO("%zu", sizeof(e1));
+  Empty2<Empty> e2{};
+  LOG_INFO("%zu", sizeof(e2));
+  Empty2<int> e3{};
+  LOG_INFO("%zu", sizeof(e3));
+  Empty4 e4;
+  LOG_INFO("%zu", sizeof(e4));
+  Empty5 e5;
+  LOG_INFO("%zu", sizeof(e5));
+}
+Empty e1;
+Empty e2;
+TEST(TEST_EMPTY, SAMPLE2) {
+  LOG_INFO("%p", &e1);
+  LOG_INFO("%p", &e2);
+}
