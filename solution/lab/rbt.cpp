e7519e0602c190ce769fc1c692fe99640590394a
AntiO2 <antio2@qq.com>
2023-11-17 21:59:11 +0800

    lab rbt

---
+++ solution/lab/rbt.cpp
@@ -0,0 +1,30 @@
+/**
+ * @author Anti
+ * @date 2023/11/16
+ * @brief rbt
+ */
+
+#include "fmt/core.h"
+#include "gtest/gtest.h"
+#include "logger.h"
+
+class Solution {
+ private:
+  std::set<int> set_;
+
+ public:
+  void init() {
+    for (int i = 0; i < 10; i++) {
+      set_.insert(i);
+    }
+  }
+};  // rbt
+
+TEST(testrbt, SAMPLE1) {
+  Solution sol;
+  sol.init();
+}
+
+TEST(rbt, SAMPLE2) {
+  Solution sol;
+}
