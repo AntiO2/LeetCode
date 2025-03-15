67ecfa66706f76347f9b75dd24fa4fc4da1f19df
AntiO2 <antio2@qq.com>
2025-03-15 23:55:36 +0800

    s3110 s3340

---
+++ leetcode-java/src/string/s3110_score_of_a_string/Solution.java
@@ -0,0 +1,13 @@
+package string.s3110_score_of_a_string;
+
+class Solution {
+    public int scoreOfString(String s) {
+        int ans = 0;
+        for(int i = 0; i < s.length() - 1; i++) {
+            char now = s.charAt(i);
+            char next = s.charAt(i + 1);
+            ans+=Math.abs(now-next);
+        }
+        return ans;
+    }
+}
---
+++ leetcode-java/src/string/s3340_check_balanced_string/Solution.java
@@ -0,0 +1,13 @@
+package string.s3340_check_balanced_string;
+
+class Solution {
+    public boolean isBalanced(String num) {
+        int diff = 0, sign = 1;
+        for(int i = 0; i < num.length(); ++i) {
+            int d = num.charAt(i) - '0';
+            diff += d* sign;
+            sign *= -1;
+        }
+        return diff == 0;
+    }
+}
